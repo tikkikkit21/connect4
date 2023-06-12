8b7efc230855054cf735eea5925d130c3b7788ad
tikkikkit21 <tikkikkit21@gmail.com>
2023-06-12 12:53:52 -0400

    Update server address to Vultr server

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import Game from './Game';
 import { io } from 'socket.io-client';
 
-const SERVER_URL = "http://localhost:1717";
+// const SERVER_URL = "http://localhost:1717";
+const SERVER_URL = "http://108.61.202.233:1717";
 const socket = io(SERVER_URL);
 
 function App() {
